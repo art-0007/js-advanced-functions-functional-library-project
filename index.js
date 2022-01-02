3c6916a3c8a9252c806d6d545269f8dac6551880
Artur <info@aigroupusa.com>
2022-01-02 17:51:44 -0500

    commit tree 559c64d5c55d7e460d6ae5ad88e1dc2f97cc1493

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const fi = (function() {
+
+  function getArray (collection) {
+      if (Array.isArray(collection) === true) {
+        return collection
+      } else if (Array.isArray(collection) === false) {
+        return Object.values(collection)
+      }
+  }
+
   return {
     libraryMethod: function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, callback) { 
+      getArray(collection).map(e => callback(e))
+      return collection
     },
 
     map: function() {
